cd0b890a8eccb9a2403b891e6df2458c7a9faaf7
SOFTSYSTEM\gkudlacz <gkudlacz@softsystem.pl>
2018-12-27 12:52:20 +0100

    init

---
+++ src/main/java/org/gk/config/RootConfig.java
@@ -0,0 +1,14 @@
+package org.gk.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages={"org.gk.business"},
+        excludeFilters={
+                @ComponentScan.Filter(type= FilterType.ANNOTATION, value=EnableWebMvc.class)
+        })
+public class RootConfig {
+}
---
+++ src/test/java/org/gk/controllers/HomeControllerTest.java
@@ -0,0 +1,25 @@
+package org.gk.controllers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class HomeControllerTest {
+
+    private HomeController homeController;
+
+    @Before
+    public void setUp() throws Exception {
+        homeController = new HomeController();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void home() {
+        assertEquals("home", homeController.home());
+    }
+}
